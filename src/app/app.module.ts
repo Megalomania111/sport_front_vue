6a0fd0ba0a3ad2f5742e466a7ca4403dc30d5595
Vadim Goncharenko <vadim1goncharenko@gmail.com>
2018-10-12 01:42:10 +0200

    add nebular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import {NbButtonModule, NbLayoutModule, NbThemeModule} from '@nebular/theme';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NbThemeModule.forRoot({ name: 'default' }),
+    RouterModule.forRoot([]), // Router is required by Nebular
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NbLayoutModule,
+    NbButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
