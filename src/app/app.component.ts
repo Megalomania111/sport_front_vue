2d09620fe9c42841eeeb6a03715e7adb801b7d3a
Vadim Goncharenko <vadim1goncharenko@gmail.com>
2018-10-11 23:27:27 +0200

    start commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ui-sport';
+  title = 'Demo';
+  greeting = {};
+  constructor(private http: HttpClient) {
+    http.get(environment.API_URL + 'resource').subscribe(data => this.greeting = data);
+  }
 }
