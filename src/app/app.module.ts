a3ad82f95af04fdf74070427224936ea97be0a0f
Darunka <vadim1goncharenko@gmail.com>
2018-10-30 08:12:09 +0100

    disable csrf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {NbButtonModule, NbLayoutModule, NbThemeModule} from '@nebular/theme';
 import {RouterModule} from '@angular/router';
 import {NbAuthModule, NbPasswordAuthStrategy} from '@nebular/auth';
 import {AppRoutingModule, routes} from './app-routing.module';
+import {environment} from '../environments/environment';
 
 
 @NgModule({
@@ -18,6 +19,27 @@ import {AppRoutingModule, routes} from './app-routing.module';
       strategies: [
         NbPasswordAuthStrategy.setup({
           name: 'email',
+          //baseEndpoint: environment.API_URL,
+          login: {
+            endpoint: 'sign-in',
+            method: 'post',
+          },
+          register: {
+            endpoint: '/auth/sign-up',
+            method: 'post',
+          },
+          logout: {
+            endpoint: '/auth/sign-out',
+            method: 'post',
+          },
+          requestPass: {
+            endpoint: '/auth/request-pass',
+            method: 'post',
+          },
+          resetPass: {
+            endpoint: '/auth/reset-pass',
+            method: 'post',
+          },
         }),
       ],
       forms: {},
